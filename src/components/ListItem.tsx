d58088ca175cfd688cacc8a8c831053f7e379b98
poohootdev <poohoot.dev@gmail.com>
2022-08-21 07:38:52 +0900

    리스트 아이템 컴포넌트 추가

---
+++ src/components/ListItem.tsx
@@ -0,0 +1,17 @@
+import { BankAccount } from '../../types/Types';
+import styles from '../../styles/Home.module.css';
+
+const ListItem = ({ data }: { data: BankAccount }) => {
+  const title = data.title;
+  const balance = data.balance;
+
+  return (
+    <div className={styles.card}>
+      <h2>
+        {title} &rarr; {balance}원
+      </h2>
+    </div>
+  );
+};
+
+export default ListItem;
