1dec40f66219dd5e0fc8f4459210c0a61ed790bd
poohootdev <poohoot.dev@gmail.com>
2022-08-21 06:16:33 +0900

    notion 가계부 잔액 내역 연동

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,19 @@
+import { Client } from '@notionhq/client';
+import { TOKEN, DATABASE_ID } from '../config';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
-const Home: NextPage = () => {
+type ListItem = {
+  id: string;
+  text: string;
+  value: number;
+};
+
+const Home: NextPage = (data: any) => {
+  // console.log(data);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -64,3 +74,52 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+export async function getStaticProps() {
+  const notion = new Client({ auth: TOKEN });
+
+  const getDatabase = () => {
+    return notion.databases.query({
+      database_id: DATABASE_ID,
+    });
+  };
+
+  const getPageTitle = (page: any) => {
+    return notion.pages.properties.retrieve({
+      page_id: page.id,
+      property_id: page.properties.name.id,
+    });
+  };
+
+  const getPageBalance = (page: any) => {
+    return notion.pages.properties.retrieve({
+      page_id: page.id,
+      property_id: page.properties.balance.id,
+    });
+  };
+
+  const getBankAccounts = async () => {
+    const db = await getDatabase();
+
+    return Promise.all(
+      db.results.map(async (product) => {
+        const title: any = await getPageTitle(product);
+        const balance: any = await getPageBalance(product);
+
+        const listItem: ListItem = {
+          id: product.id,
+          text: title.results[0].title.plain_text,
+          value: balance.property_item.rollup.number,
+        };
+
+        return listItem;
+      }),
+    );
+  };
+
+  const bankAccounts = await getBankAccounts();
+
+  return {
+    props: { bankAccounts },
+  };
+}
